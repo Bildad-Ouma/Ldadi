3c93a590f97b1d85bc25a88fd751f95164628b01
Bildad-Ouma <sirbildadbrian@gmail.com>
2025-06-30 12:00:04 +0300

    commit Voste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,7 +45,7 @@ function App(){
         <Route path='/' element={<Landing />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/dashboard' element={<Render />} />
+        <Route path='/dashboard' element={<gRender />} />
       </Routes>
       <Render/>
     </div>
